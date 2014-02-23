d69611a2dea303e75cd200d94ea84d5f00e09f58
Joshua Peterson <petersonjm1@gmail.com>
2014-02-23 08:15:14 -0500

    Reorganize branch for deployment.

--- README.md
+++
@@ -1,8 +0,0 @@
-Math Facts
-==========
-
-This is the source code for a website which creates math fact practice tests. It uses the following other software:
-
-* [Dart](http://dartlang.org)
-* [MathJax](http://mathjax.org)
-* [Bootstrap](http://getbootstrap.com)
--- lib/sum_formatter.dart
+++
@@ -1,9 +0,0 @@
-library mathfacts.sum_formatter;
-
-import 'binary_operands.dart';
-
-class SumFormatter {
-  String format(BinaryOperands operands) {
-    return "\\begin{array}{cc} & ${operands.left} \\\\ + & ${operands.right} \\\\ \\hline \\\\ \\end{array}";
-  }
-}
